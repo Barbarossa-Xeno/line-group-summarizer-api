0e8a78946909d8e26389e5e593a59aec6577a41c
Barbarossa_Xeno <060812ez.tm@gmail.com>
2026-09-05 21:33:23 +0900

    一旦Gemini無効化

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { http } from '@google-cloud/functions-framework';
 import { GoogleGenAI } from '@google/genai';
-import * as line from "@line/bot-sdk";
+import { validateSignature, LineBotClient } from "@line/bot-sdk";
+
+const client = LineBotClient.fromChannelAccessToken({
+		channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
+	});
 
 // エントリーポイント
 http('main', async (req, res) => {
@@ -14,31 +18,34 @@ http('main', async (req, res) => {
     `);
 	}
 
-  const isValid = line.validateSignature(req.rawBody, process.env.LINE_API_SECRET, req.headers['x-line-signature']);
+  // LINEの署名を検証
+  const isValid = validateSignature(req.rawBody, process.env.LINE_CHANNEL_SECRET, req.headers['x-line-signature']);
 
+  // 署名が有効でない場合（LINE外からのアクセス）は拒否
 	if (!isValid) {
 		return res.status(401).send("Unauthorized");
 	}
 
+	// TODO: イベント処理
 	for (const event of req.body.events) {
 		console.log(event);
 	}
 
-	// 200だけ送って終わらせない
+	// LINEのサーバー側には200だけ送って終わらせない
 	res.status(200).send("OK");
 
-	//
-  try {
-    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-    const response = await ai.models.generateContent({
-      model: 'gemini-3.5-flash',
-      contents: 'あなたのできることを1文で簡単に教えて。'
-    });
+	// 本処理
+  // try {
+  //   const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+  //   const response = await ai.models.generateContent({
+  //     model: 'gemini-3.5-flash',
+  //     contents: 'あなたのできることを1文で簡単に教えて。'
+		// });
 
-    res.send(response.text);
-  }
-  catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  }
+  //   return res.send(response.text);
+  // }
+  // catch (error) {
+  //   console.error(error);
+  //   return res.status(500).send(error);
+  // }
 });
